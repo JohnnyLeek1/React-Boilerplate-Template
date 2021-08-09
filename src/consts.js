cd0b0812d9adfe2cd8d2d0c4e06abea3cad2fb14
johnnyleek <leek1j@cmich.edu>
2021-08-09 12:29:55 -0400

    React Github Pages working (using conditional HashRouter)

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+// CHANGE THESE TO FIT YOUR PROJECT
+const BASENAME = '/React-Boilerplate-Template';
+const IS_PROJECT_PAGE = true;
+
+export { BASENAME, IS_PROJECT_PAGE };
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import '../styles/app.scss';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
 
 import Hello from '../components/Hello';
 import Page404 from '../components/404';
+import { BASENAME, IS_PROJECT_PAGE } from '../consts';
+import PropTypes from 'prop-types';
+
+const Router = ({ children }) => {
+  return IS_PROJECT_PAGE ? (
+    <HashRouter basename={`${BASENAME}/#/`}>{children}</HashRouter>
+  ) : (
+    <BrowserRouter basename={BASENAME}>{children}</BrowserRouter>
+  );
+};
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/hello" component={Hello} />
         <Route path="/" exact component={Hello} />
         <Route path="/" component={Page404} />
       </Switch>
     </Router>
   );
 }
+
+Router.propTypes = {
+  children: PropTypes.any,
+};
 
 export default App;
